d40a3d2b38a514e312283591ccca36e8118679a2
lanss-id <maulanakayyis354@gmail.com>
2024-08-04 08:06:36 +0700

    feat: implement auth, appwrrite and sentry

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,14 +1,11 @@
 import HeaderBox from "@/components/HeaderBox";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-const home = () => {
-  const loggedIn = {
-    firstName: "Maulana",
-    lastName: "Kayyis",
-    email: "maulanakayyis354@gmail.com",
-  };
+const home = async () => {
+  const loggedIn = await getLoggedInUser();
   return (
     <section className="home">
       <div className="home-content">
@@ -16,7 +13,7 @@ const home = () => {
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || "Guest"}
+            user={loggedIn?.name || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
           />
 
